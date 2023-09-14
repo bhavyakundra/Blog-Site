f6e81809b68ff5b082045cc697c59f9ca9265548
andyroediger <73503684+andyroediger@users.noreply.github.com>
2023-09-14 13:47:33 -0700

    Blog Link 091423

--- flask_blog_andy/flaskblog/posts/routes.py
+++ flask_blog_andy/flaskblog/posts/routes.py
@@ -33,7 +33,9 @@ def send_notification(post, users):
         <body>
             <h1>New Post</h1>
             <h2>{{ post.title }}</h2>
+            <p><a href="{{  }}">Blog</a> Blog Link</p>
             <p><a href="{{ unsubscribe_url }}">Click here</a> to unsubscribe .</p>
+                            
         </body>
         </html>
         """)
