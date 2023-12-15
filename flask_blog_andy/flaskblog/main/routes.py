3a9287e0dc54e700a763910e2074793026d3590e
andyroediger <73503684+andyroediger@users.noreply.github.com>
2023-12-15 08:29:19 -0800

    upload dummy price

--- flask_blog_andy/flaskblog/main/routes.py
+++ flask_blog_andy/flaskblog/main/routes.py
@@ -3,7 +3,7 @@ from flaskblog.models import Post
 from flask import render_template,request
 from flaskblog.posts.forms import PostForm
 from flask_login import current_user, login_required
-import yfinance as yf
+# import yfinance as yf
 main = Blueprint('main', __name__)
 
 
@@ -32,6 +32,9 @@ def mary():
 
 @main.route("/models")
 def models():
-    price = yf.Ticker("HSAI")
-    current_ticker = price.info['currentPrice']
-    return render_template('models.html', HSAI_price=current_ticker)
+    return render_template('models.html', HSAI_price=2.3)
+
+# def models():
+#     price = yf.Ticker("HSAI")
+#     current_ticker = price.info['currentPrice']
+#     return render_template('models.html', HSAI_price=current_ticker)
